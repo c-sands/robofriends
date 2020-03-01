3fcd3ba6ad1bc9db6e58a1ff5075385a3f4bdca1
c-sands <csandsxd@gmail.com>
2020-02-29 19:14:46 -0500

    changed size of Scroll component

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Scroll = (props) => {
 	return(
-		<div style={{overflowY: 'scroll', height: '800px', padding: '10px', borderTop: '5px solid #000000'}}>
+		<div style={{overflowY: 'scroll', height: '600px', padding: '10px', borderTop: '5px solid #000000'}}>
 			{props.children}
 		</div>
 	)
