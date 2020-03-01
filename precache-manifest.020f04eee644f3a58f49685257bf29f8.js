15a1e159b86a61d67b429201e830f9a8e85f093b
c-sands <csandsxd@gmail.com>
2020-02-29 19:35:47 -0500

    Updates

---
+++ precache-manifest.020f04eee644f3a58f49685257bf29f8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba87fa58fe3c77094cf5dee12fc89cb8",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "eabd1a8b852b8b505d0b",
+    "url": "/react-gh-pages/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "0fd653300a5294bf7b98",
+    "url": "/react-gh-pages/static/css/main.3bd1efa5.chunk.css"
+  },
+  {
+    "revision": "eabd1a8b852b8b505d0b",
+    "url": "/react-gh-pages/static/js/2.a5747d58.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-gh-pages/static/js/2.a5747d58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fd653300a5294bf7b98",
+    "url": "/react-gh-pages/static/js/main.582b01eb.chunk.js"
+  },
+  {
+    "revision": "ef299473892feefc9dd6",
+    "url": "/react-gh-pages/static/js/runtime-main.2c35d896.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/react-gh-pages/static/media/SEGA.12a3db8b.woff"
+  }
+]);
